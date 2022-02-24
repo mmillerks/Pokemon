714436a8a8946531563f248b045e9a9a54cb9f88
Mindy Miller <mmillerks@gmail.com>
2022-02-23 22:00:19 -0600

    index.jsx view rendered at pokemon route

--- software_classwork/unit_2/pokemon_app/models/pokemon.js
+++ software_classwork/unit_2/pokemon_app/models/pokemon.js
@@ -7,3 +7,22 @@ const pokemon = [
     {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
     {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
  ];
+
+
+//Set up your 'database' so that it can be exported to your server.js and then be required by your server.js
+module.exports = pokemon;
+
+
+//Index
+app.get('/pokemon', function(req, res) {
+    Pokemon.find({}, (err, "") => {
+        if (err) {
+            res.status(400).send(err)
+        } else {
+            res.render('pokemon/Index', {
+                keyvalue: pair
+            })
+        }
+    })
+});
+//Sooo, I don't know exactly what I'm doing here. But I think I started it correctly???
